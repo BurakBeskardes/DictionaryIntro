fa7509d9d2de97513a1ca2e04d653f4d64b3f79b
burakbk7 <burakbk7@gmail.com>
2021-01-27 00:06:44 +0300

    Add project files.

---
+++ DictionaryIntro/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryIntro
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Dictionary<int, string> MyDictionary = new Dictionary<int, string>();
+
+            MyDictionary.Add(1907, "FENERBAHÇE");
+            MyDictionary.Add(1905, "GALATASARAY");
+            MyDictionary.Add(1903, "BEŞİKTAŞ");
+            MyDictionary.Add(1967, "TRABZONSPOR");
+
+            foreach (var dictionary in MyDictionary)
+            {
+                Console.WriteLine("Kuruluş tarihi : " + dictionary.Key + '\n' + "Takım :  " + dictionary.Value);
+            }
+
+
+        }
+    }
+}
